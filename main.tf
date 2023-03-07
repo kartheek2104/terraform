ad7a38cc593feff3ad750182a6b173d23a2c56e2
kartheek <kartheek210498@gmail.com>
2023-03-07 08:38:15 +0530

    modify .gitignore and main.tf

--- main.tf
+++ main.tf
@@ -3,10 +3,24 @@ provider "aws" {
   //shared_credentials_files = "C:/Users/SREEJA/.aws/credentials" 
   }
 
-resource "aws_vpc" "dev-vpc" {
-  cidr_block = "30.0.0.0/24"
+variable vpc_cidr_block {}
+variable subnet_cidr_block {}
+variable avail_zone {}
+variable env_prefix {}
+
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_block
   tags = {
-    Name = "from-tf"
+    Name: "${var.env_prefix}-vpc"
   }
 }
 
+resource "aws_subnet" "name" {
+   vpc_id = aws_vpc.myapp-vpc.id
+   cidr_block = var.subnet_cidr_block
+   availability_zone = var.avail_zone
+
+   tags = {
+    Name: "${var.env_prefix}-subnet-1" 
+   }
+}

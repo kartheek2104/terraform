74376f0e68a31415d70b46d87e4a9d91cfc1ff08
kartheek <kartheek210498@gmail.com>
2023-03-07 09:10:38 +0530

    add route table and igw

--- main.tf
+++ main.tf
@@ -1,26 +1,47 @@
 provider "aws" {
   region = "ap-south-1"
   //shared_credentials_files = "" 
-  }
+}
 
-variable vpc_cidr_block {}
-variable subnet_cidr_block {}
-variable avail_zone {}
-variable env_prefix {}
+variable "vpc_cidr_block" {}
+variable "subnet_cidr_block" {}
+variable "avail_zone" {}
+variable "env_prefix" {}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
-    Name: "${var.env_prefix}-vpc"
+    Name : "${var.env_prefix}-vpc"
   }
 }
 
 resource "aws_subnet" "myapp-subnet-1" {
-   vpc_id = aws_vpc.myapp-vpc.id
-   cidr_block = var.subnet_cidr_block
-   availability_zone = var.avail_zone
+  vpc_id            = aws_vpc.myapp-vpc.id
+  cidr_block        = var.subnet_cidr_block
+  availability_zone = var.avail_zone
 
-   tags = {
-    Name: "${var.env_prefix}-subnet-1" 
-   }
+  tags = {
+    Name : "${var.env_prefix}-subnet-1"
+  }
+}
+
+resource "aws_route_table" "myapp-route-table" {
+  vpc_id = aws_vpc.myapp-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+
+  }
+  tags = {
+    Name : "${var.env_prefix}-rtb"
+  }
+}
+
+
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  tags = {
+    Name : "${var.env_prefix}-igw"
+  }
 }

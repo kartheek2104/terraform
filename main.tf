30ec58488564c32a9255484a118e286cafb557f6
kartheek <kartheek210498@gmail.com>
2023-03-07 11:01:01 +0530

    add ec2

--- main.tf
+++ main.tf
@@ -8,6 +8,7 @@ variable "subnet_cidr_block" {}
 variable "avail_zone" {}
 variable "env_prefix" {}
 variable "my_ip" {}
+variable "instance_type" {}
 
 resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
@@ -103,3 +104,41 @@ resource "aws_security_group" "myapp-sg" {
   }
 
 }
+
+data "aws_ami" "latest-amazon-linux-image" {
+  
+  most_recent = true
+  owners = ["amazon"]
+
+  filter{
+    name = "name"
+    values = ["amzn2-ami-kernel-5.10-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+  
+}
+
+# this will display the output of ami id 
+/*
+output "aws_ami_id" {
+  value = data.aws_ami.latest-amazon-linux-image.id
+  
+} */
+
+resource "aws_instance" "myapp-server" {
+  ami = data.aws_ami.latest-amazon-linux-image.id
+  instance_type = var.instance_type
+  subnet_id = aws_subnet.myapp-subnet-1.id
+  vpc_security_group_ids = [aws_security_group.myapp-sg.id]
+  availability_zone = var.avail_zone
+  associate_public_ip_address = true
+  key_name = "server-key"
+
+  tags = {
+    Name : "${var.env_prefix}-server"
+  }
+} 
